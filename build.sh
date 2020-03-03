4fe8d907d850d616e26324923a642b036702aa8c
A.R.C <admin@arc.co>
2020-03-03 10:09:24 +0800

    200303 improve build script

--- build.sh
+++ build.sh
@@ -13,40 +13,48 @@ if [ ! $1 ]; then
   exit
 fi
 
-mkdir ./res/converted
-mkdir ./res/converted/data
-mkdir ./res/converted/data/imposts
-mkdir ./res/converted/data/osfiles
-mkdir ./res/converted/data/subtitles
-mkdir ./res/converted/audios
-mkdir ./res/converted/audios/bgms
-mkdir ./res/converted/audios/sounds
-mkdir ./res/converted/audios/story
-mkdir ./res/converted/audios/extra
-mkdir ./res/converted/images
-mkdir ./res/converted/images/gallery
-mkdir ./res/converted/images/imfiles
-mkdir ./res/converted/images/osfiles
-mkdir ./res/converted/images/imavatars
-mkdir ./res/converted/images/osavatars
-mkdir ./res/converted/images/osspecial
+if [ ! -d "./res/converted" ]; then
+  mkdir ./res/converted
+fi
+
+if [ ! -d "./res/converted/data" ]; then
+  mkdir ./res/converted/data
+  mkdir ./res/converted/data/imposts
+  mkdir ./res/converted/data/osfiles
+  mkdir ./res/converted/data/subtitles
+fi
+
+if [ ! -d "./res/converted/audios" ]; then
+  mkdir ./res/converted/audios
+  mkdir ./res/converted/audios/bgms
+  mkdir ./res/converted/audios/sounds
+  mkdir ./res/converted/audios/story
+  mkdir ./res/converted/audios/extra
+fi
+
+if [ ! -d "./res/converted/images" ]; then
+  mkdir ./res/converted/images
+  mkdir ./res/converted/images/gallery
+  mkdir ./res/converted/images/imfiles
+  mkdir ./res/converted/images/osfiles
+  mkdir ./res/converted/images/imavatars
+  mkdir ./res/converted/images/osavatars
+  mkdir ./res/converted/images/osspecial
+fi
 
 # script
 python parse.py $1
 
-# static
-cp -r ./res/export/images ./web/public
-cp -r ./res/export/videos ./web/public
-cp -r ./res/converted/data ./web/public
-cp -r ./res/converted/audios ./web/public
-cp -r ./res/converted/images ./web/public
+# build dist
+if [ $2 == "dist" ]; then
+  cp -r ./res/export/images ./web/public
+  cp -r ./res/export/videos ./web/public
+  cp -r ./res/converted/data ./web/public
+  cp -r ./res/converted/audios ./web/public
+  cp -r ./res/converted/images ./web/public
 
-# raw assets
-cp -r ./res/export/assets/game/15_os/bundleassets/osstickers ./web/public/images
+  # raw assets
+  cp -r ./res/export/assets/game/15_os/bundleassets/osstickers ./web/public/images
 
-if [ ! $2 ]; then
-  echo "Website was built."
-  exit
+  cd ./web && npm i && npm run build
 fi
-
-cd ./web && npm i && npm run build
